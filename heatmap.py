cb67157b769cec8bfc046a2ee0d981d0a3e4e521
Morgan Fine-Morris <mfinemorris@icloud.com>
2015-05-12 15:26:43 -0700

    added ability to pass an axis to the heatmap function. Also changed how boundaries for colorscaling plus sundry changes to the if __name__=='__main__' section.

--- heatmap.py
+++ heatmap.py
@@ -30,7 +30,7 @@ def show_values(pc, fmt="%.2f", **kw):
     from itertools import izip
     pc.update_scalarmappable()
     ax = pc.get_axes()
-    print pc.get_array()
+    #print pc.get_array()
     for p, color, value in izip(pc.get_paths(), pc.get_facecolors(), pc.get_array()):
         x, y = p.vertices[:-2, :].mean(0)
         if np.all(color[:3] > 0.5):
@@ -40,20 +40,27 @@ def show_values(pc, fmt="%.2f", **kw):
         ax.text(x, y, fmt % value, ha="center", va="center", color=color, **kw)
 
 
-def heatmap_from_array(data, colors = plt.cm.Blues, x_labels=[], y_labels=[], colorbar_label='', annotate_function=show_values, vmin_vmax=None):
+def heatmap_from_array(data, ax = None, colors = plt.cm.Blues, x_labels=[], y_labels=[], colorbar_label='', annotate_function=show_values, vmin_vmax=None):
     '''
     The annotate function should have defaults for all args but the pcolorplot arg set to desired values. (how should I change this to make it better?)
     To remove annotation pass annotate_function=lambda pcolorplot,fmt='': ''
     
     '''
+    if not ax:
+        ax = plt.gca()
     
-    fig, ax = plt.subplots()
     
     if vmin_vmax:
         v_min,v_max = vmin_vmax
         cax = ax.pcolor(data, cmap=colors, vmin=v_min, vmax=v_max)
+        cbar_tick_label = np.linspace(v_min,v_max,10)#int(dmin),int(dmax))#[dmin, dmin+(dmax-dmin)/2, dmax]
+
     else:
-        cax = ax.pcolor(data, cmap=colors)
+        #determine min and max for data
+        dmin, dmax = round(data.min(),2),round(data.max(),2)#np.floor(data.min()), np.ceil(data.max())
+        cax = ax.pcolor(data, cmap=colors, vmin=dmin, vmax=dmax)
+        cbar_tick_label = np.linspace(dmin,dmax,10)#int(dmin),int(dmax))#[dmin, dmin+(dmax-dmin)/2, dmax]
+
     annotate_function(cax)
     '''
     try:
@@ -68,8 +75,11 @@ def heatmap_from_array(data, colors = plt.cm.Blues, x_labels=[], y_labels=[], co
     ax.set_xticklabels(x_labels, minor=False)
     ax.set_yticklabels(y_labels, minor=False)
 
-    dmin, dmax = round(data.min(),3), round(data.max(),3)
-    cbar_tick_label = [dmin, dmin+(dmax-dmin)/2, dmax]
+    #set colorbar labels based on plot min and max values
+    #print dmin, dmax, (dmax-dmin)
+    #print
+    #cbar_tick_label = np.linspace(dmin,dmax,10)#int(dmin),int(dmax))#[dmin, dmin+(dmax-dmin)/2, dmax]
+    #print cbar_tick_label
     cbar = plt.colorbar(cax, ticks=cbar_tick_label)
     cbar.set_label(colorbar_label)
     cbar.set_ticklabels([round(i,2) for i in cbar_tick_label])
@@ -77,7 +87,7 @@ def heatmap_from_array(data, colors = plt.cm.Blues, x_labels=[], y_labels=[], co
     return plt.gca(), cbar
 
 
-def heatmap_from_dataframe(dataframe, colors = plt.cm.Blues, colorbar_label = '', annotate_function = show_values, vmin_vmax = None):
+def heatmap_from_dataframe(dataframe, ax=None, colors = plt.cm.Blues, colorbar_label = '', annotate_function = show_values, vmin_vmax = None):
     '''
     To remove annotation pass annotate_function=lambda pcolorplot,fmt='': ''
 
@@ -86,7 +96,7 @@ def heatmap_from_dataframe(dataframe, colors = plt.cm.Blues, colorbar_label = ''
     label1, label2 = dataframe.columns.values, dataframe.index.values
     data = np.array(dataframe.values)
     
-    return heatmap_from_array(data, colors, label1, label2, colorbar_label,  annotate_function, vmin_vmax)
+    return heatmap_from_array(data, ax, colors, label1, label2, colorbar_label,  annotate_function, vmin_vmax)
 
 
 
@@ -103,14 +113,24 @@ if __name__ == '__main__':
     test_index = [1.8, 2.4, 3.0, 3.6, 4.2]
     test_columns = [u'-65.0', u'-60.0', u'-55.0']
     
-    ax, colorbar = heatmap_from_array(np.array(test_data), x_labels = test_columns, y_labels = test_index, colorbar_label = measure+' (s)', annotate_function = show_values, vmin_vmax=(0,4.))
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    
+    ax1_2, colorbar1 = heatmap_from_array(np.array(test_data), ax1, x_labels = test_columns, y_labels = test_index, colorbar_label = measure+' (s)', annotate_function = show_values, vmin_vmax=(0,4.))
 
-    ax.set_title(measure)
-    ax.set_xlabel(r'eL (mV)')
-    ax.set_ylabel(r'$g_{nap}$ (nS)')
-    plt.show()
+    colorbar1.remove()
+
+    ax1.set_title(measure)
+    ax1.set_xlabel(r'eL (mV)')
+    ax1.set_ylabel(r'$g_{nap}$ (nS)')
+    #plt.show()
+    #sys.exit(0)
+
+    ax2 = fig.add_subplot(121)
 
     df = pd.DataFrame(data = test_data, index = test_index, columns = test_columns)
-    heatmap_from_dataframe(df, colorbar_label = measure+' (s)', annotate_function = show_values)
+    ax2_2, colorbar2 = heatmap_from_dataframe(df, ax=ax2, colorbar_label = measure+' (s)', annotate_function = show_values)
+    
+    plt.tight_layout()
     plt.show()
 

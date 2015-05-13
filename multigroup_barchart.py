52b6a30bd43241d1be89c8c48651d6c7ec7d0873
Morgan Fine-Morris <mfinemorris@icloud.com>
2015-05-13 16:13:13 -0700

    multigroup_barchart: added default color handling.

--- multigroup_barchart.py
+++ multigroup_barchart.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 
-def bar_chart(mean_lists, std_lists, group_labels, tick_labels, colors_list=None, set_separation=.5, side_buffer=0.1):
+def bar_chart(mean_lists, std_lists, group_labels, tick_labels, colors_list=[], set_separation=.5, side_buffer=0.1):
     '''
     modified from http://matplotlib.org/examples/api/barchart_demo.html (May 10, 2015)
     
@@ -32,6 +32,16 @@ def bar_chart(mean_lists, std_lists, group_labels, tick_labels, colors_list=None
     plt.show()
     
     -------------------------------------------------------------------------------------
+    future work:
+    not satisfied with method of setting color scheme
+    see that you can use izip longest to leave colors as an empty array
+    
+    and then set it later:
+    
+    >>>colormap = plt.cm.gist_ncar #see choosing colormaps at http://matplotlib.org/users/colormaps.html
+    >>>plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, num_plots)])
+    >>>for n, (means, stds, labels) in enumerate(itertools.izip_longest(means, std, group_labels)):
+        ...
     '''
     
     def autolabel(rects):
@@ -45,7 +55,15 @@ def bar_chart(mean_lists, std_lists, group_labels, tick_labels, colors_list=None
     num_groups = len(mean_lists)
     
     if not colors_list:
-        colors_list = ['b' for i in np.arange(num_groups)]
+        colormap = plt.cm.rainbow  #gist_ncar
+        colors_list=[colormap(i) for i in np.linspace(0, 0.9, num_groups)]
+    
+    #check that lists are the same length
+    lists = [std_lists, group_labels, colors_list]
+    for list in lists:
+        if len(mean_lists) != len(list):
+            raise Exception('mean_list, std_lists, group_labels and colors_list must all be of the same length. they are {} long respectively.'.format(map(len, lists)))
+    
     
     width=1.0/(num_groups+set_separation)
     ind = np.arange(N)+set_separation  # the x locations for the groups. note each value is the left most edge of a bar.
@@ -56,7 +74,7 @@ def bar_chart(mean_lists, std_lists, group_labels, tick_labels, colors_list=None
     x_min = np.inf
     for n, (means, stds, labels, color) in enumerate(itertools.izip(mean_lists, std_lists, group_labels, colors_list)):
         left = ind+(n*width)
-        bar_set = ax.bar(left, means, width, color=color, yerr=stds, label=labels)
+        bar_set = ax.bar(left, means, width, color=color, yerr=stds, label=labels) #add bar graphs
         autolabel(bar_set)
         bar_groups.append(bar_set)
         if x_max < left[-1]:
@@ -89,8 +107,10 @@ if __name__ == '__main__':
     stds = [menStd, womenStd, intersexStd]
 
     # 3 group bar chart
-    ax = bar_chart(means,stds,('Men','Women','Intersex'),('G1', 'G2', 'G3', 'G4', 'G5'), colors)
+    ax = bar_chart(means,stds,('Men','Women','Intersex'),('G1', 'G2', 'G3', 'G4', 'G5'))#, colors)
     ax.legend(loc=8)
+    plt.show()
+    
     
     # 2 group bar chart
     ax = bar_chart(means[:2],stds[:2],('Men','Women'),('G1', 'G2', 'G3', 'G4', 'G5'), colors[:2])
